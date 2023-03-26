4ac876b7ffbb1b93e0f3a77a1560af6682e7d616
Ryan <ryan@loudfoot.ca>
2023-03-26 19:12:51 -0400

    Fix and update the ChatGPT cog

--- cogs/chatgpt.py
+++ cogs/chatgpt.py
@@ -8,18 +8,26 @@ import openai
 class ChatGPTCog(commands.Cog):
     def __init__(self, bot, openai_api_token):
         self.bot = bot
-        self.openai_api_token = openai_api_token
+        openai.api_key = openai_api_token
 
     @commands.command()
     async def gpt(self, ctx, *, prompt):
-        if not config.openai_api_token:
+        if not openai.api_key:
             await ctx.reply('The config file does not have a value for openai_api_token.')
             return
 
         response = openai.Completion.create(
-            engine="davinci", prompt=prompt, max_tokens=50
+            engine="davinci", prompt=prompt + "\nAnswer the question as accurately as possible without any extra info.", max_tokens=100
         )
-        await ctx.reply(response.choices[0].text)
+
+        # Get the first text choice from the response
+        text = response.choices[0].text.strip()
+
+        # Check if the response is empty or contains an error message
+        if not text or text.startswith("Error:"):
+            await ctx.reply("I'm sorry, I could not generate a response.")
+        else:
+            await ctx.reply(text)
 
 async def setup(bot):
     await bot.add_cog(ChatGPTCog(bot, config.openai_api_token))
