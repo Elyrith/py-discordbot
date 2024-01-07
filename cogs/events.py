3c8703a2690cd68e63bf0731accfc6b3bf10e433
Ryan <ryan@loudfoot.ca>
2024-01-07 00:27:07 -0500

    Revert "Load configs from files and store in a variable now. Now require /reload of cog if config changes, but will save on disk IO."
    This reverts commit fe90c2ee6124bbd0ca90c673b23c846aab592e96.

--- cogs/events.py
+++ cogs/events.py
@@ -13,6 +13,16 @@ from discord.ext import commands, tasks
 
 log = logging.getLogger('discord')
 
+# Load YAML files once when bot starts
+guild_configs = {}
+for filename in os.listdir("./cogs/events_config"):
+    if filename == ".gitignore" or filename == "000000000000000000.yaml":
+        continue # Ignore .gitignore and the template file.
+    with open(f"./cogs/events_config/{filename}", "r") as file:
+        guild_id = int(filename.split(".")[0])
+        guild_configs[guild_id] = yaml.safe_load(file)
+
+
 class EventsCog(commands.Cog):
     """Events stuff."""
     def __init__(self, bot) -> None:
@@ -23,28 +33,24 @@ class EventsCog(commands.Cog):
     # Post about events at various intervals: 24, 1, and 0 hours remaining.
     @tasks.loop(minutes=1)
     async def post_about_events(self) -> None:
+        # If the current minute is not 00, 15, 30, or 45, return without doing anything
+        current_minute = datetime.now().minute
+        if current_minute not in [14, 29, 44, 59]:
+            return
+
+        # Otherwise, continue...
         try:
-            guilds = self.bot.guilds
-            for guild in guilds:
+            for guild in self.bot.guilds:
                 # Find out if we should even bother getting the list of events. The fetch_scheduled_events function is really slow, so we only want to run it for guilds we don't have a config file for.
 
-                # Get the events channel
-                guild_id = guild.id
-
                 # Skip this guild if there's no config file for it.
-                filename = f"./cogs/events_config/{guild_id}.yaml"
-                if not os.path.isfile(filename):
-#                    log.info(f"No file found. {filename} at {os.getcwd()}")
+                guild_id = guild.id
+                guild_config = guild_configs.get(guild_id)
+                if not guild_config:
                     continue
 
-                try:
-                    with open(filename, "r") as file:
-                        data = yaml.safe_load(file)
-                        events_channel = data['events_channel']
-                        channel = self.bot.get_channel(events_channel)
-                except Exception as e:
-                    log.error(f"Exception in post_about_events loop when accessing YAML file: {e.__class__.__name__}: {e}")
-                    return
+                events_channel = guild_config.get("events_channel")
+                channel = self.bot.get_channel(events_channel)
 
                 # Fetch the scheduled events for the guild. We do this because guilds.scheduled_events doesn't get updated after the bot has started. The fetch is slow, though.
                 guild_events = await guild.fetch_scheduled_events()
@@ -71,13 +77,11 @@ class EventsCog(commands.Cog):
                     # Only if the event is scheduled.
                     if event.status == discord.EventStatus.scheduled or event.status == discord.EventStatus.active:
                         if hours_until_start == 24 or hours_until_start == 1:
-                            print(f"[{event.name}]({event.url}) is starting in {hours_until_start} hours. ({event_start_time})")
+                            log.info(f"Message posted: [{event.name}]({event.url}) is starting in {hours_until_start} hours. ({event_start_time})")
                             await channel.send(f"[{event.name}]({event.url}) is starting in {hours_until_start} hours. ({event_start_time})")
                         elif hours_until_start == 0:
-                            print(f"[{event.name}]({event.url}) is starting now. ({event_start_time})")
+                            log.info(f"Message posted: [{event.name}]({event.url}) is starting now. ({event_start_time})")
                             await channel.send(f"[{event.name}]({event.url}) is starting now. ({event_start_time})")
-#                            await event.start()
-                            log.info(f"Started event: {event.name} at {event.start_time}")
         except Exception as e:
             await log.error(f"Exception in post_about_events loop: {e}.")
 
